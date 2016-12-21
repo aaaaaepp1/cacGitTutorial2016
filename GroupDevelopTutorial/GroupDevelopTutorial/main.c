c59aaddc5547165e4d8e373cb8368b2537d811a2
伊藤永光 <itounagamitsu@itounagamitsu-no-MacBook-Air.local>
2016-12-21 18:16:53 +0900

    mainの制作

--- GroupDevelopTutorial/GroupDevelopTutorial/main.c
+++ GroupDevelopTutorial/GroupDevelopTutorial/main.c
@@ -24,11 +24,11 @@ int WindowWidth, WindowHeight;
 //物理演算とかする
 void fpsHandler() {
 	
-//	for(int i = 0; i < model_list.length; i++) {
-//		Solid3D tmp = model_list.at(&model_list, i);
-//		tmp.setMoveVec(&tmp, Vector3_init(0.1, 0, 0));
-//	}
-//	model_list.allUpdate(&model_list);
+	for(int i = 0; i < model_list.length; i++) {
+		Solid3D tmp = model_list.at(&model_list, i);
+		tmp.setMoveVec(&tmp, Vector3_init(0.1, 0, 0));
+	}
+	model_list.allUpdate(&model_list);
 	
 }
 
@@ -62,31 +62,31 @@ int init() {
 
 int main(int argc, const char * argv[]) {
 	
-//	//ウィンドウサイズ初期化
-//	WindowWidth = 960;
-//	WindowHeight = 540;
-//	
-//	
-//	//init variable
-//	tutorialView_initVariables(&WindowWidth, &WindowHeight, &model_list);
-//	model_list = List_Solid3d_init();
-//	model_list.add(&model_list, Solid3D_init_Cube(Vector3_init(0.0, 0.0, 0.0), Vector3_init(0.0, 0.0, 0.0), 2.0, 2.0, 2.0));
-//	
-//	
-//	//GLUTの制御関数
-//	glutInitWindowPosition(0, 0);
-//	glutInitWindowSize(WindowWidth, WindowHeight);
-//	glutInit(&argc, argv);
-//	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH);
-//	glutCreateWindow("Tutorial");
-//	glutDisplayFunc(displayHandler);
-//	glutReshapeFunc(displayReshapeFunc);
-//	glutMouseFunc(mouseController);
-//	glutPassiveMotionFunc(mousePositionHandler);
-//	glutKeyboardFunc(keyboardController);
-//	glutKeyboardUpFunc(keyboardController_Up);
-//	init();
-//	glutMainLoop();
-//	
-//    return 0;
+	//ウィンドウサイズ初期化
+	WindowWidth = 960;
+	WindowHeight = 540;
+	
+	
+	//init variable
+	tutorialView_initVariables(&WindowWidth, &WindowHeight, &model_list);
+	model_list = List_Solid3d_init();
+	model_list.add(&model_list, Solid3D_init_Cube(Vector3_init(0.0, 0.0, 0.0), Vector3_init(0.0, 0.0, 0.0), 2.0, 2.0, 2.0));
+	
+	
+	//GLUTの制御関数
+	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(WindowWidth, WindowHeight);
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH);
+	glutCreateWindow("Tutorial");
+	glutDisplayFunc(displayHandler);
+	glutReshapeFunc(displayReshapeFunc);
+	glutMouseFunc(mouseController);
+	glutPassiveMotionFunc(mousePositionHandler);
+	glutKeyboardFunc(keyboardController);
+	glutKeyboardUpFunc(keyboardController_Up);
+	init();
+	glutMainLoop();
+	
+    return 0;
 }
